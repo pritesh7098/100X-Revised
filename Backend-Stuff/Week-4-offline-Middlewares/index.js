8d0eb22abf206ba0de4c8af1102fb8ca2434fa36
Pritesh7098 <Priteshdhanad2001@gmail.com>
2025-11-19 09:50:07 +0530

    Added express middlware logic

--- Backend-Stuff/Week-4-offline-Middlewares/index.js
+++ Backend-Stuff/Week-4-offline-Middlewares/index.js
@@ -39,10 +39,40 @@ app.get("/ride2", function (req, res) {
 
 // ----------------------------------------------------------
 
+// applying  express middlewares
+// 1- age checker middleware
 
+function isOldEnoughMiddleware(req, res, next) {
+  const age = req.query.age;
 
+  if (age > 14) {
+    next();
+  } else {
+    res.json({
+      msg: "You are not of age yet",
+    });
+  }
+}
 
+// remember - you can define mw indivisually or as an seprate fun like below.
+// also it only triggers for the routes defined below that so use it wisely.
+
+app.use(isOldEnoughMiddleware);
+
+app.get("/ride1", function (req, res) {
+  res.json({
+    msg: "you have successfully riden the ride 1",
+  });
+});
+
+app.get("/ride2", function (req, res) {
+  res.json({
+    msg: "you have successfully riden the ride 2",
+  });
+});
 
 const port = 3000;
 
 app.listen(port);
+
+// cont from asignment solving based on mw
