8ca92d857a62dc909882e478b052c8d0518bfe5a
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-03-09 11:10:13 +0530

    understood multiple http req with query, params, body

--- Backend From First Principles/Week3/Day2/query-params-body-project/index.js
+++ Backend From First Principles/Week3/Day2/query-params-body-project/index.js
@@ -8,9 +8,8 @@ const users = [
   {
     id: 1,
     Name: "pritesh",
-    role: "Admin",
+    role: "admin",
   },
-
   {
     id: 2,
     Name: "Rahul",
@@ -18,14 +17,16 @@ const users = [
   },
 ];
 
-/* Actual code  */
+// 1) Req.query
 
-// 1) Req
+ app.get("/", (req, res) => {
+  res.send("Server working");
+});
 
-app.get("./users", (req, res) => {
+/*app.get("/users", (req, res) => {
   const { role } = req.query;
 
-  const result = users;
+  let result = users;
 
   if (role) {
     result = users.filter((u) => u.role === role);
@@ -33,7 +34,44 @@ app.get("./users", (req, res) => {
 
   res.json(result);
 });
+ */
+
+// 2) Req.params
+
+ app.get("/users/:id", (req, res) => {
+  let userId = Number(req.params.id);
+
+  let user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
+
+  res.json(user);
+}); 
+
+// 3) Req.body
+
+app.post("/users", (req, res) => {
+  const { name, role } = req.body;
+
+  if (!name || !role) {
+    return res.status(404).json({
+      message: "name and role are the required field",
+    });
+  }
+
+  const newUser = {
+    id: users.length + 1,
+    name,
+    role,
+  };
+
+  users.push(newUser);
+
+  res.status(201).json(newUser);
+});
 
 app.listen(3000, () => {
-  console.log("Server is running on port : 3000");
+  console.log("Server running on port 3000");
 });
