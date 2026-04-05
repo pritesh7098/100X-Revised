361d5c0557f667bc4fbc1f4979be60cbe98c7799
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-04-05 12:02:35 +0530

    Understood how we can connect nodeJS to Postgres and perform a CRUD Operations.

---
+++ Backend From First Principles/Phase3-Databases/nodeProject-postgres/dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+/* const pgClient = new Client("psql 'postgresql://neondb_owner:npg_YMfEB8iW7ewJ@ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'") */
+// alternate approach 
+const pgClient = new pg_1.Client({
+    user: "neondb_owner",
+    password: "npg_YMfEB8iW7ewJ",
+    port: 5432,
+    host: "ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech",
+    database: "neondb",
+    ssl: true,
+});
+// from here you can perform crud/query db without having any console. 
+async function main() {
+    await pgClient.connect();
+    const responese = await pgClient.query("select * from users where id=3");
+    console.log(responese);
+}
+main();
+//# sourceMappingURL=index.js.map
--- Backend From First Principles/Phase3-Databases/nodeProject-postgres/src/index.ts
+++ Backend From First Principles/Phase3-Databases/nodeProject-postgres/src/index.ts
@@ -2,7 +2,7 @@ import { Client } from "pg";
 
 /* const pgClient = new Client("psql 'postgresql://neondb_owner:npg_YMfEB8iW7ewJ@ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'") */
 
-// alternate approach
+// alternate approach 
 
 const pgClient = new Client({
   user: "neondb_owner",
@@ -10,13 +10,16 @@ const pgClient = new Client({
   port: 5432,
   host: "ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech",
   database: "neondb",
+  ssl: true,
 });
+
+
+// from here you can perform crud/query db without having any console. 
 
 async function main() {
   await pgClient.connect();
-  const responese = await pgClient.query("select * from users");
+  const responese = await pgClient.query("select * from users where id=3");
   console.log(responese);
-  
 }
 
-main()
+main();
