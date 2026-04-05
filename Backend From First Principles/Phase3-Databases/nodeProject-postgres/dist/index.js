60d73e386a8e44eb69f479a9c4718d68329dc3c8
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-04-05 12:57:45 +0530

    Added express logic to deal with pg via node and express

--- Backend From First Principles/Phase3-Databases/nodeProject-postgres/dist/index.js
+++ Backend From First Principles/Phase3-Databases/nodeProject-postgres/dist/index.js
@@ -1,8 +1,42 @@
 "use strict";
+// Traditional Way
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+/* import { Client } from "pg";
+
+/* const pgClient = new Client("psql 'postgresql://neondb_owner:npg_YMfEB8iW7ewJ@ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'") /
+
+// alternate approach
+
+const pgClient = new Client({
+  user: "neondb_owner",
+  password: "npg_YMfEB8iW7ewJ",
+  port: 5432,
+  host: "ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech",
+  database: "neondb",
+  ssl: true,
+});
+
+
+// from here you can perform crud/query db without having any console.
+
+async function main() {
+  await pgClient.connect();
+  const responese = await pgClient.query("select * from users where id=3");
+  console.log(responese);
+}
+
+main();
+ */
+/* -------------------------------------------------- */
+// With express
+const express_1 = __importDefault(require("express"));
 const pg_1 = require("pg");
-/* const pgClient = new Client("psql 'postgresql://neondb_owner:npg_YMfEB8iW7ewJ@ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'") */
-// alternate approach 
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+// DB Connection
 const pgClient = new pg_1.Client({
     user: "neondb_owner",
     password: "npg_YMfEB8iW7ewJ",
@@ -11,11 +45,28 @@ const pgClient = new pg_1.Client({
     database: "neondb",
     ssl: true,
 });
-// from here you can perform crud/query db without having any console. 
-async function main() {
-    await pgClient.connect();
-    const responese = await pgClient.query("select * from users where id=3");
-    console.log(responese);
-}
-main();
+// connecting to db
+pgClient.connect();
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "You are on homepage",
+    });
+});
+app.post("/signup", async (req, res) => {
+    const userName = req.body.userName;
+    const email = req.body.email;
+    const password = req.body.password;
+    // Insert Query
+    const insertQuery = `INSERT INTO users (userName,email,password) VALUES (${userName}, ${email}, ${password})`;
+    const responese = await pgClient.query(insertQuery);
+    res.status(201).json({
+        success: true,
+        message: "You have signup successfully",
+    });
+});
+// -----------------------------------------------------
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server is running at port : 3000");
+});
 //# sourceMappingURL=index.js.map
--- Backend From First Principles/Phase3-Databases/nodeProject-postgres/src/index.ts
+++ Backend From First Principles/Phase3-Databases/nodeProject-postgres/src/index.ts
@@ -1,6 +1,8 @@
-import { Client } from "pg";
+// Traditional Way
 
-/* const pgClient = new Client("psql 'postgresql://neondb_owner:npg_YMfEB8iW7ewJ@ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'") */
+/* import { Client } from "pg";
+
+/* const pgClient = new Client("psql 'postgresql://neondb_owner:npg_YMfEB8iW7ewJ@ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'") /
 
 // alternate approach 
 
@@ -23,3 +25,71 @@ async function main() {
 }
 
 main();
+ */
+
+/* -------------------------------------------------- */
+
+// With express
+
+import express from "express";
+
+import { Client } from "pg";
+
+const app = express();
+app.use(express.json());
+
+// DB Connection
+
+const pgClient = new Client({
+  user: "neondb_owner",
+  password: "npg_YMfEB8iW7ewJ",
+  port: 5432,
+  host: "ep-restless-moon-am7qb659-pooler.c-5.us-east-1.aws.neon.tech",
+  database: "neondb",
+  ssl: true,
+});
+
+// connecting to db
+
+pgClient.connect();
+
+
+// route 1 
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "You are on homepage",
+  });
+});
+
+// route 2
+
+app.post("/signup", async (req, res) => {
+  const userName = req.body.userName;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Insert Query
+
+  const insertQuery = `INSERT INTO users 
+  (userName,email,password) 
+  VALUES (${userName}, ${email}, ${password})`;
+
+  const response = await pgClient.query(insertQuery);
+
+  res.status(201).json({
+    success: true,
+    message: "You have signup successfully",
+  });
+});
+
+// -----------------------------------------------------
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server is running at port : 3000");
+});
+
+
+// -----------------------------------------------------
