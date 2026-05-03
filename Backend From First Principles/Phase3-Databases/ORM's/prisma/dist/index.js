42f28811791afdea57863c079c46b8af5f17fe36
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-05-03 11:33:24 +0530

    updated some configs

--- Backend From First Principles/Phase3-Databases/ORM's/prisma/dist/index.js
+++ Backend From First Principles/Phase3-Databases/ORM's/prisma/dist/index.js
@@ -1,15 +1,41 @@
 console.log("HI");
 import { PrismaClient } from "@prisma/client";
+import { idText } from "typescript";
 const client = new PrismaClient();
+// You can do any crud operation here like inserting, updating
+//delting etc and it will convert to SQL query eventually and fill the db.
 async function createUser() {
     await client.user.create({
         data: {
             username: "Pritesh",
-            passwor: "12345679",
+            password: "12345679",
             age: 24,
             city: "pune",
         },
     });
 }
 createUser();
+async function updateUser() {
+    await client.user.update({
+        where: {
+            id: 1,
+        },
+        data: {
+            password: "12345",
+        },
+    });
+}
+updateUser();
+async function getUserData() {
+    const user = await client.user.findFirst({
+        where: {
+            id: 1,
+        },
+        include: {
+            todos: true,
+        },
+    });
+    console.log(user);
+}
+getUserData();
 //# sourceMappingURL=index.js.map
--- Backend From First Principles/Phase3-Databases/ORM's/prisma/src/index.ts
+++ Backend From First Principles/Phase3-Databases/ORM's/prisma/src/index.ts
@@ -1,14 +1,18 @@
 console.log("HI");
 
 import { PrismaClient } from "@prisma/client";
+import { idText } from "typescript";
 
 const client = new PrismaClient();
+
+// You can do any crud operation here like inserting, updating
+//delting etc and it will convert to SQL query eventually and fill the db.
 
 async function createUser() {
   await client.user.create({
     data: {
       username: "Pritesh",
-      passwor: "12345679",
+      password: "12345679",
       age: 24,
       city: "pune",
     },
@@ -16,3 +20,37 @@ async function createUser() {
 }
 
 createUser();
+
+async function updateUser() {
+  await client.user.update({
+    where: {
+      id: 1,
+    },
+
+    data: {
+      password: "12345",
+    },
+  });
+}
+
+updateUser();
+
+
+// relationships - one to many - one user has many todo's so print it. 
+
+async function getUserData() {
+  const user = await client.user.findFirst({
+    where: {
+      id: 1,
+    },
+
+    include: {
+      todos: true,
+    },
+  });
+
+  console.log(user);
+  
+
+}
+getUserData();
