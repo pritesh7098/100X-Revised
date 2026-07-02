e394bfdce2452016d876681e91448f7bdba8bc23
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-07-02 13:27:03 +0530

    Learned about the function is TS

--- Frontend/Typescript/class3/src/index.ts
+++ Frontend/Typescript/class3/src/index.ts
@@ -1,8 +1,43 @@
-let a:number = 2; 
-console.log(a);
+/* let a: number = 2;
+console.log(a); */
 
 /* ------------------ */
 
-// Some advance typescript API's 
+// Some advance typescript API's
 
-// Pick - 
+// Pick -
+
+/* ------------- */
+
+// Functions in TS
+
+function makeChai(type: string, cups: number) {
+  console.log(`making a ${cups} cups of ${type} chai `);
+}
+
+makeChai("masalaChai", 2);
+
+// logger function
+
+function logChai(): void {
+  console.log(
+    "This is void fun which is never giving data outside, as it's void function",
+  );
+}
+
+// function with parameter
+
+function orderChai(type: string = "masala") {
+  console.log();
+}
+
+// slightly complicated syntax
+
+function makeCha2(order: {
+  type: string;
+  sugar: number;
+  size: "large" | "small";
+}): number {
+  return 4; // in case if you are giving type to function
+  //  console.log(3); // in case you are not giving type to fun
+}
